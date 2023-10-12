f85e353b5dac2bfcec7105baf9b5a2260d3cad9e
Valentino Alonso <valenalonso7@gmail.com>
2023-10-12 20:44:19 -0300

    commit pre entrega 2

--- archivo.js
+++ archivo.js
@@ -1,33 +1,48 @@
-function calcularNotaFinal() {
-    const notas = document.querySelectorAll(".nota");
-    let suma = 0;
-    let cantidadNotas = 0;
+class Estudiante {
+    constructor() {
+        this.notas = [];
+    }
 
-    for (let i = 0; i < notas.length; i++) {
-        const nota = parseFloat(notas[i].value);
+    agregarNota(nota) {
+        if (!isNaN(nota)) {
+            this.notas.push(parseFloat(nota));
+        } else {
+            throw new Error("Por favor, ingrese un valor numérico válido en todas las notas.");
+        }
+    }
 
-        if (isNaN(nota)) {
-            document.getElementById("resultado").innerHTML = "Por favor, ingrese valores numéricos válidos en todas las notas.";
-            return;
+    calcularNotaFinal() {
+        if (this.notas.length === 0) {
+            return "No se ingresaron notas.";
         }
 
-        suma += nota;
-        cantidadNotas++;
+        const suma = this.notas.reduce((acumulador, nota) => acumulador + nota, 0);
+        const notaFinal = suma / this.notas.length;
+
+        const mensaje = `La nota final es: ${notaFinal.toFixed(2)}. `;
+        if (notaFinal >= 4) {
+            return mensaje + "El estudiante ha aprobado.";
+        } else {
+            return mensaje + "El estudiante ha reprobado.";
+        }
     }
+}
 
-    if (cantidadNotas === 0) {
-        document.getElementById("resultado").innerHTML = "No se ingresaron notas.";
-        return;
+const estudiante = new Estudiante();
+
+function agregarNota() {
+    const notaInput = document.getElementById("nota");
+    const nota = notaInput.value;
+
+    try {
+        estudiante.agregarNota(nota);
+        notaInput.value = ""; // Limpiar el campo de entrada
+    } catch (error) {
+        document.getElementById("resultado").innerHTML = error.message;
     }
+}
 
-    const notaFinal = suma / cantidadNotas;
-
-    let mensaje = `La nota final es: ${notaFinal.toFixed(2)}. `;
-    if (notaFinal >= 4) {
-        mensaje += "El estudiante ha aprobado.";
-    } else {
-        mensaje += "El estudiante ha reprobado.";
-    }
-
-    document.getElementById("resultado").innerHTML = mensaje;
+function calcularNotaFinal() {
+    const resultado = estudiante.calcularNotaFinal();
+    document.getElementById("resultado").innerHTML = resultado;
 }
